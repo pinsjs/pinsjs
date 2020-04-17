29818e6b657ec02e574df770fbee5ca38f6b2046
Javier Luraschi <javierluraschi@hotmail.com>
2020-04-17 16:43:54 -0700

    properly assert arrays to be equal

--- tests/test_pins.py
+++ tests/test_pins.py
@@ -3,7 +3,7 @@ import os
 import pins
 
 def test_board_list_default():
-    assert pins.board_list() == ["local", "packages"]
+    assert all([a == b for a, b in zip(pins.board_list(), ["local", "packages"])])
 
 def test_sources():
     template = os.path.join(pins.__path__[0], 'js', 'pins.js')
