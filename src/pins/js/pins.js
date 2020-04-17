ec3934c7e3c9ff70d9731088d0bb753dffd31a7d
Javier Luraschi <javierluraschi@hotmail.com>
2020-04-16 22:00:07 -0700

    sync pins.js

--- src/pins/js/pins.js
+++ src/pins/js/pins.js
@@ -1,105 +1,109 @@
-var pins = (function (exports) {
+"use strict";
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+var pins = function (exports) {
   'use strict';
 
-  const getOption = () => null;
+  var getOption = function getOption() {
+    return null;
+  };
 
-  const unique = (arr) => {
-    function onlyUnique(value, index, self) { 
+  var unique = function unique(arr) {
+    function onlyUnique(value, index, self) {
       return self.indexOf(value) === index;
     }
 
     return arr.filter(onlyUnique);
   };
 
-  const copy = (source) => {
-    var copied = [];
-    for (var i = 0; i < source.length; i++) {
-      copied[i] = source[i];
-    }
-
-    return copied;
-  };
-
-  const append = (arr, value) => {
-    var appended = copy(arr);
+  var concat = function concat(arr, value) {
     if (value !== null) {
-      appended.push(value);
+      arr = arr.concat(value);
     }
 
-    return appended;
+    return arr;
   };
 
-  const BOARDS_REGISTERED = {};
+  var BOARDS_REGISTERED = {};
 
-  const list = () => Object.keys(BOARDS_REGISTERED);
+  var list = function list() {
+    return Object.keys(BOARDS_REGISTERED);
+  };
 
-  const boardInfer = (x, { name, board, registerCall, connect, url }) => {
+  var boardInfer = function boardInfer(x, _ref) {
+    var name = _ref.name,
+        board = _ref.board,
+        registerCall = _ref.registerCall,
+        connect = _ref.connect,
+        url = _ref.url;
     throw 'NYI';
   };
 
-  const boardConnect = (board, code, ...args) => {
+  var boardConnect = function boardConnect(board, code) {
     var board = boardGet(board);
-
     uiViewerRegister(board, code);
-
     return board;
   };
 
-  const boardDisconnect = (name, ...args) => {
+  var boardDisconnect = function boardDisconnect(name) {
     throw 'NYI';
   };
 
-  const boardList = () => {
-    var defaults = append(["local", "packages"], boardDefault());
-    var boards = append(list(), defaults);
-
+  var boardList = function boardList() {
+    var defaults = concat(['local', 'packages'], boardDefault());
+    var boards = concat(list(), defaults);
     return unique(boards);
   };
 
-  const boardGet = (name) => {
+  var boardGet = function boardGet(name) {
     if (name === null) {
       name = boardDefault();
     }
 
-    registerCall <- "pins::board_register(board = \"" + name + "\")";
+    registerCall < -'pins::board_register(board = "' + name + '")';
 
     if (!list().includes(name)) {
       var boardInferred = boardInfer();
 
       if (boardInferred['registerCall'] !== null) {
         registerCall = boardInferred['registerCall'];
-      }
+      } // attempt to automatically register board
 
-      // attempt to automatically register board
+
       name = boardInferred['name'];
+
       try {
-        boardRegister(
-          board_inferred['board'],
-          {
-            name: boardInferred['name'],
-            connect: boardInferred['connect'],
-            registerCall: registerCall,
-            url: board_inferred['url']
-          });
-      }
-      catch(err) {
-      }
+        boardRegister(board_inferred['board'], {
+          name: boardInferred['name'],
+          connect: boardInferred['connect'],
+          registerCall: registerCall,
+          url: board_inferred['url']
+        });
+      } catch (err) {}
 
       if (!list().includes(name)) {
-        throw "Board '" + name + "' not a board, available boards: " + boardList().join(", ");
+        throw "Board '" + name + "' not a board, available boards: " + boardList().join(', ');
       }
     }
   };
 
-  const boardRegister = (board, { name, cache, versions, ...args }) => {
+  var boardRegister = function boardRegister(board, _ref2) {
+    var name = _ref2.name,
+        cache = _ref2.cache,
+        versions = _ref2.versions,
+        args = _objectWithoutProperties(_ref2, ["name", "cache", "versions"]);
+
     throw 'NYI';
   };
 
-  const boardDeregister = (name, ...args) => {
+  var boardDeregister = function boardDeregister(name) {
     throw 'NYI';
   };
 
-  const boardDefault = () => {
+  var boardDefault = function boardDefault() {
     return getOption();
   };
 
@@ -110,7 +114,5 @@ var pins = (function (exports) {
   exports.boardGet = boardGet;
   exports.boardList = boardList;
   exports.boardRegister = boardRegister;
-
   return exports;
-
-}({}));
+}({});
