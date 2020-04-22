fec429d34e1c6cf9fbfb07094a1637c339aab681
Javier Luraschi <javierluraschi@hotmail.com>
2020-04-22 11:33:23 -0700

    improve tests

--- src/pins/__init__.py
+++ src/pins/__init__.py
@@ -7,31 +7,36 @@ import js2py
 _logger = logging.getLogger(__name__)
 
 def host_log(message):
-    _logger.info(message)
+  _logger.info(message)
 
 def setup_logging(log_level):
-    log_format = "[%(asctime)s] %(levelname)s:%(name)s %(message)s"
-    logging.basicConfig(level=log_level, stream=sys.stdout,
-                        format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
+  log_format = "[%(asctime)s] %(levelname)s:%(name)s %(message)s"
+  logging.basicConfig(level=log_level, stream=sys.stdout,
+                      format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
 
 def pins_configure():
-    global _context
-    global _pins_lib
+  global _context
+  global _pins_lib
 
-    setup_logging("INFO")
-    _logger.debug("pins starting...")
+  setup_logging("INFO")
+  _logger.debug("pins starting...")
 
-    pins_path = os.path.join(__path__[0], 'js', 'pins.js')
+  pins_path = os.path.join(__path__[0], "js", "pins.js")
 
-    file = open(pins_path, mode = 'r')
-    pins_source = file.read()
-    file.close()
+  file = open(pins_path, mode = "r")
+  pins_source = file.read()
+  file.close()
 
-    _context = js2py.EvalJs({ 'host_log': host_log })
-    _pins_lib = _context.eval(pins_source)
+  _context = js2py.EvalJs({ "host_log": host_log })
+  _pins_lib = _context.eval(pins_source)
 
 pins_configure()
 
 def board_list():
-    global _pins_lib
-    return _pins_lib['boardList']()
+  global _pins_lib
+  return _pins_lib["boardList"]()
+
+def callbacks_set(name, callback):
+  global _pins_lib
+  _pins_lib["callbacks"]["set"](name, callback);
+  return True;
--- tests/test_pins.py
+++ tests/test_pins.py
@@ -2,9 +2,23 @@ import pytest
 import os
 import pins
 
-def test_board_list_default():
-    assert all([a == b for a, b in zip(pins.board_list(), ["local", "packages"])])
+def assert_array_equals(value, expected):
+  assert all([a == b for a, b in zip(value, expected)])
 
 def test_sources():
-    template = os.path.join(pins.__path__[0], 'js', 'pins.js')
-    assert os.path.exists(template)
+  template = os.path.join(pins.__path__[0], "js", "pins.js")
+  assert os.path.exists(template)
+
+def test_board_list_default():
+  assert_array_equals(pins.board_list(), ["local", "packages"])
+
+def _callback_tests(option):
+  options = {
+      "pins.board": "memory"
+  }
+  return options[option]
+
+def test_callbacks():
+  pins.callbacks_set("getOption", _callback_tests)
+  assert_array_equals(pins.board_list(), ["local", "packages", "memory"])
+  pins.callbacks_set("getOption", null)
