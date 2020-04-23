70891ce900ef59e96f190287854d5035fc153b19
Javier Luraschi <javierluraschi@hotmail.com>
2020-04-23 15:26:23 -0700

    split callbacks into browser and node.js

---
+++ hosts/pins-nodejs.js
@@ -0,0 +1,13 @@
+/*
+ * Provides defaults callbacks for Node.js
+ */
+
+pins.callbacks.set("dirCreate", function() { return null; });
+pins.callbacks.set("dirExists", function() { return null; });
+pins.callbacks.set("tempfile", function() { return null; });
+pins.callbacks.set("readLines", function() { return null; });
+pins.callbacks.set("basename", function() { return null; });
+pins.callbacks.set("boardRegisterCode", function() { return null; });
+pins.callbacks.set("uiViewerRegister", function() { return null; });
+pins.callbacks.set("userCacheDir", function(name) { return "pins/" + name; });
+pins.callbacks.set("pinLog", function(message) { console.log(message); });
