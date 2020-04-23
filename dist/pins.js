994f379be1c5dfead73600038c2502a8460c72e2
Javier Luraschi <javierluraschi@hotmail.com>
2020-04-23 13:07:00 -0700

    rebuild pins.js

--- dist/pins.js
+++ dist/pins.js
@@ -4,9 +4,7 @@ var pins = (function (exports) {
   var HOST_CALLBACKS = {};
 
   var get = function (name) {
-    var callback = HOST_CALLBACKS[name];
-
-    if (callback) { return callback; }
+    if (Object.keys(HOST_CALLBACKS).includes(name)) { return HOST_CALLBACKS[name]; }
     else { return function () {
       var args = [], len = arguments.length;
       while ( len-- ) args[ len ] = arguments[ len ];
@@ -24,7 +22,10 @@ var pins = (function (exports) {
     set: set,
   };
 
-  var getOption = function (name) { return get('getOption')(name); };
+  var getOption = function (name, defval) {
+    var option = get('getOption')(name);
+    return option != null ? option : defval;
+  };
 
   var unique = function (arr) {
     function onlyUnique(value, index, self) {
@@ -52,6 +53,14 @@ var pins = (function (exports) {
     BOARDS_REGISTERED[name] = board;
   };
 
+  var userCacheDir = function () {
+    return callbacks.get("userCacheDir")("pins");
+  };
+
+  var boardCachePath = function () {
+    return getOption("pins.path", userCacheDir());
+  };
+
   var dir = Object.freeze({
     create: function create(dirPath, ref) {
       if ( ref === void 0 ) ref = { recursive: false };
@@ -74,7 +83,8 @@ var pins = (function (exports) {
     var args = [], len = arguments.length - 2;
     while ( len-- > 0 ) args[ len ] = arguments[ len + 2 ];
 
-    if (!dir.exists(board['cache'])) { dir.create(board['cache'], { recursive: true }); }
+    if (!dir.exists(board['cache']))
+      { dir.create(board['cache'], { recursive: true }); }
 
     return board;
   };
@@ -85,6 +95,15 @@ var pins = (function (exports) {
 
     // TODO: UseMethod("board_initialize")
     return boardInitializeLocal(board, args['cache'], args);
+  };
+
+  var pinLog = function () {
+    var args = [], len = arguments.length;
+    while ( len-- ) args[ len ] = arguments[ len ];
+
+    if (getOption('pins.verbose', true)) {
+      callbacks.get('pinLog')(args.join(""));
+    }
   };
 
   function objectWithoutProperties (obj, exclude) { var target = {}; for (var k in obj) if (Object.prototype.hasOwnProperty.call(obj, k) && exclude.indexOf(k) === -1) target[k] = obj[k]; return target; }
@@ -103,7 +122,12 @@ var pins = (function (exports) {
       class: board,
     };
 
-    board = boardInitialize(board, (cache = cache), (versions = versions), args);
+    board = boardInitialize(
+      board,
+      (cache = cache),
+      (versions = versions),
+      args
+    );
 
     return board;
   };
@@ -124,7 +148,7 @@ var pins = (function (exports) {
     };
 
     // if boards starts with http:// or https:// assume this is a website board
-    if ((/^http:\/\/|^https:\/\//gi).test(x)) {
+    if (/^http:\/\/|^https:\/\//gi.test(x)) {
       inferred['url'] = x;
       inferred['board'] = 'datatxt';
 
@@ -223,6 +247,11 @@ var pins = (function (exports) {
     var rest = objectWithoutProperties( ref, ["name", "cache", "versions"] );
     var args = rest;
 
+    pinLog("[boardRergister] Registering '", board, "'' board with name '", name, "'");
+
+    if (name == null) { name = board; }
+    if (cache == null) { cache = boardCachePath(); }
+
     var inferred = boardInfer(board, {
       board: board,
       name: name,
@@ -231,7 +260,7 @@ var pins = (function (exports) {
       url: args['url'],
     });
 
-    args['url'] = inferred["url"];
+    args['url'] = inferred['url'];
     board = newBoard(inferred['board'], inferred['name'], cache, versions, args);
 
     set$1(inferred['name'], board);
@@ -256,7 +285,7 @@ var pins = (function (exports) {
   };
 
   var boardDefault = function () {
-    return getOption('pins.board');
+    return getOption('pins.board', 'local');
   };
 
   exports.boardConnect = boardConnect;
