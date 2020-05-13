f1b69eab4a4ede84b658240d6073d1243b21e1a9
Javier Luraschi <javierluraschi@hotmail.com>
2020-05-13 11:40:16 -0700

    format main.js without linter

--- src/main.js
+++ src/main.js
@@ -11,53 +11,17 @@ import * as boardExtensions from './board-extensions';
 import * as boardLocal from './board-local';
 
 inheritance.registerMethod('pin', 'default', pinDefault.pinDefault);
-inheritance.registerMethod(
-  'pinPreview',
-  'default',
-  pinDefault.pinPreviewDefault
-);
+inheritance.registerMethod('pinPreview', 'default', pinDefault.pinPreviewDefault);
 inheritance.registerMethod('pinLoad', 'default', pinDefault.pinLoadDefault);
 inheritance.registerMethod('pinFetch', 'default', pinDefault.pinFetchDefault);
 
-inheritance.registerMethod(
-  'boardBrowse',
-  'default',
-  boardExtensions.boardBrowse
-);
-inheritance.registerMethod(
-  'boardPinVersions',
-  'default',
-  boardExtensions.boardPinVersions
-);
-inheritance.registerMethod(
-  'boardInitialize',
-  'default',
-  boardExtensions.boardInitializeDefault
-);
+inheritance.registerMethod('boardBrowse', 'default', boardExtensions.boardBrowse);
+inheritance.registerMethod('boardPinVersions', 'default', boardExtensions.boardPinVersions);
+inheritance.registerMethod('boardInitialize', 'default', boardExtensions.boardInitializeDefault);
 
-inheritance.registerMethod(
-  'boardInitialize',
-  'local',
-  boardLocal.boardInitializeLocal
-);
-inheritance.registerMethod(
-  'boardPinCreate',
-  'local',
-  boardLocal.boardPinCreateLocal
-);
-inheritance.registerMethod(
-  'boardPinFind',
-  'local',
-  boardLocal.boardPinFindLocal
-);
+inheritance.registerMethod('boardInitialize', 'local', boardLocal.boardInitializeLocal);
+inheritance.registerMethod('boardPinCreate', 'local', boardLocal.boardPinCreateLocal);
+inheritance.registerMethod('boardPinFind', 'local', boardLocal.boardPinFindLocal);
 inheritance.registerMethod('boardPinGet', 'local', boardLocal.boardPinGetLocal);
-inheritance.registerMethod(
-  'boardPinRemove',
-  'local',
-  boardLocal.boardPinRemoveLocal
-);
-inheritance.registerMethod(
-  'boardPinVersions',
-  'local',
-  boardLocal.boardPinVersionsLocal
-);
+inheritance.registerMethod('boardPinRemove', 'local', boardLocal.boardPinRemoveLocal);
+inheritance.registerMethod('boardPinVersions', 'local', boardLocal.boardPinVersionsLocal);
