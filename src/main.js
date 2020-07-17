581ba794d5d8ab9d6eb6e01eb03fcd4988e6ffdb
Javier Luraschi <javierluraschi@hotmail.com>
2020-07-17 16:17:41 -0700

    minor var rename

--- src/main.js
+++ src/main.js
@@ -11,13 +11,15 @@ import * as boardExtensions from './board-extensions';
 import * as boardLocal from './board-local';
 import { pinString } from './pin-file';
 import { pinLoadFiles } from './pin-files';
+import { pinDataFrame } from './pin-dataframe';
 
 inheritance.registerMethod('pin', 'default', pinDefault.pinDefault);
+inheritance.registerMethod('pin', 'string', pinString);
+inheritance.registerMethod('pin', 'dataframe', pinDataFrame);
+
 inheritance.registerMethod('pinPreview', 'default', pinDefault.pinPreviewDefault);
 inheritance.registerMethod('pinLoad', 'default', pinDefault.pinLoadDefault);
 inheritance.registerMethod('pinFetch', 'default', pinDefault.pinFetchDefault);
-
-inheritance.registerMethod('pin', 'string', pinString);
 
 inheritance.registerMethod('pinLoad', 'files', pinLoadFiles);
 
