f490b6a22a374f523b579737a80c099ede147a73
Javier Luraschi <javierluraschi@hotmail.com>
2020-08-31 12:25:40 -0700

    draft parameterizable board tests

--- python/tests/board_test.py
+++ python/tests/board_test.py
@@ -1,15 +1,31 @@
 import pins
-import os
 import tempfile
-import shutil
-import pathlib
 import pytest
+import random
+import os
 
-boardTests = [
+board_tests = [
     ("local"),
 ]
 
+random_file_index = random.randint(1, 1000)
+temp_file = tempfile.TemporaryDirectory().name
 
-@pytest.mark.parametrize("board", boardTests)
-def test_timedistance_v0(board):
+pin_name = 'afile' + str(random_file_index)
+dataset_name = 'adataset' + str(random_file_index)
+
+@pytest.mark.parametrize("board", board_tests)
+def test_initialize_test(board):
+    text_file_path = "fixtures/" + board + "/files/hello.txt"
+
+    os.makedirs("fixtures/" + board + "/files", exist_ok = True)
+
+    out_file = open(text_file_path, "w")
+    out_file.writelines(["hello world"])
+    out_file.close()
+
+    assert True
+
+@pytest.mark.parametrize("board", board_tests)
+def test_can_pin_file(board):
     assert True
