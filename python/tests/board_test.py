0bb20cb15638ae539ad2f2934bfedb9bac267b81
Javier Luraschi <javierluraschi@hotmail.com>
2020-08-31 12:56:12 -0700

    draft test_can_pin_file test

--- python/tests/board_test.py
+++ python/tests/board_test.py
@@ -14,13 +14,18 @@ temp_file = tempfile.TemporaryDirectory().name
 pin_name = 'afile' + str(random_file_index)
 dataset_name = 'adataset' + str(random_file_index)
 
+def configuration_test(board):
+    return {
+        "text_file_path": "fixtures/" + board + "/files/hello.txt"
+    }
+
 @pytest.mark.parametrize("board", board_tests)
 def test_initialize_test(board):
-    text_file_path = "fixtures/" + board + "/files/hello.txt"
+    config = configuration_test(board)
 
-    os.makedirs("fixtures/" + board + "/files", exist_ok = True)
+    os.makedirs("fixtures/" + board + "/files", exist_ok=True)
 
-    out_file = open(text_file_path, "w")
+    out_file = open(config["text_file_path"], "w")
     out_file.writelines(["hello world"])
     out_file.close()
 
@@ -28,4 +33,14 @@ def test_initialize_test(board):
 
 @pytest.mark.parametrize("board", board_tests)
 def test_can_pin_file(board):
-    assert True
+    config = configuration_test(board)
+
+    cached_path = pins.pin(config["text_file_path"], name=pin_name, board=board)
+
+    assert len(cached_path) > 0
+
+    # in_file = open(config["text_file_path"], "w")
+    # lines = in_file.readlines ()
+    # in_file.close()
+
+    # assert lines == ['hello world'];
