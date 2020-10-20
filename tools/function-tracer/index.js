86b623f4facc0e0ef37151194858469b1615ede4
Javier Luraschi <javierluraschi@hotmail.com>
2020-10-20 12:00:19 -0700

    add support for parameters in tracer util

--- tools/function-tracer/index.js
+++ tools/function-tracer/index.js
@@ -1,1 +1,8 @@
-console.log("Adding traces...");
+const yargs = require("yargs");
+
+const options = yargs
+ .usage("Usage: -f <name>")
+ .option("f", { alias: "file", describe: "The file to trace", type: "string", demandOption: true })
+ .argv;
+
+console.log(`Tracing ${options.file}`);
---
+++ tools/function-tracer/tests/expression.js
@@ -0,0 +1,2 @@
+const answer = 42;
+
