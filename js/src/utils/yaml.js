edbf50536bb280b8e45e2d9f257facc05534c32b
Javier Luraschi <javierluraschi@hotmail.com>
2020-12-17 23:19:12 -0800

    avoid using yaml callbacks in js

--- js/src/utils/yaml.js
+++ js/src/utils/yaml.js
@@ -3,10 +3,9 @@ import callbacks from '../host/callbacks';
 import { pinLog } from '../log';
 
 export function safeDump(data) {
-  const yamlSafeDump = callbacks.get('yamlSafeDump');
-  if (yamlSafeDump) {
+  if (callbacks.has('yamlSafeDump')) {
     // data is passed as json to avoid unsupported object serialization to python
-    return yamlSafeDump(JSON.stringify(data));
+    return callbacks.get('yamlSafeDump')(JSON.stringify(data));
   }
 
   // remove undnefineds which are unsupported by jsyaml but cause issues in python
@@ -16,8 +15,9 @@ export function safeDump(data) {
 }
 
 export function safeLoad(text) {
-  const yamlSafeLoad = callbacks.get('yamlSafeLoad');
-  if (yamlSafeLoad) return yamlSafeLoad(text);
+  if (callbacks.has('yamlSafeLoad')) {
+    return callbacks.get('yamlSafeLoad')(text);
+  }
 
   return yaml.safeLoad(text);
 }
